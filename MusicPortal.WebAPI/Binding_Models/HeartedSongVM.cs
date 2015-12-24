291c56e2463fe01b42646256809010f86244d78a
SumejjaPorca <sumejja93@gmail.com>
2015-12-24 16:23:37 +0100

    VMs changed and some added

--- MusicPortal.WebAPI/Binding_Models/HeartedSongVM.cs
+++ MusicPortal.WebAPI/Binding_Models/HeartedSongVM.cs
@@ -1,26 +1,18 @@
-﻿using MusicPortal.WebAPI.Domain_Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace MusicPortal.WebAPI.Binding_Models
 {
-    public class SongVM {
-        public int Id { get; set; }
+    public class HeartedSongVM
+    {
+        public string UserId { get; set; }
+        public int SongId { get; set; }
         public string Name { get; set; }
-
+        public bool IsHearted { get; set; }      
         public string Link { get; set; }
         public string AuthorNames { get; set; } //TO DO: get authors name when returning this view model
-
-        public SongVM() {
-
-        }
-
-        public SongVM(Song song) {
-            this.Id = song.Id;
-            this.Name = song.Name;
-        }
 
     }
 }
