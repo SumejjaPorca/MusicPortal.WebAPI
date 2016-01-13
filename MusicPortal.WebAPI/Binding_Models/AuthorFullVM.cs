c63fc3056e21b525c60552accd4daea38e385e69
pejovicluka454@gmail.com <pejovicluka454@gmail.com>
2016-01-13 21:46:35 +0100

    Author i publish

---
+++ MusicPortal.WebAPI/Binding_Models/AuthorFullVM.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPortal.WebAPI.Binding_Models
+{
+    public class AuthorFullVM
+    {
+        [JsonProperty(PropertyName = "id")]
+        public long Id { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "songs")]
+        public IEnumerable<HeartedSongVM> Songs { get; set; }
+    }
+}
---
+++ MusicPortal.WebAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,46 @@
+﻿using MusicPortal.WebAPI.Binding_Models;
+using MusicPortal.WebAPI.BL;
+using MusicPortal.WebAPI.Domain_Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MusicPortal.WebAPI.Controllers
+{
+    public class AuthorsController : ApiController
+    {
+        Data.MusicPortalDbContext _db = new Data.MusicPortalDbContext();
+
+        //GET api/authors/54516
+        [Route("{id}")]
+        [Authorize]
+        [HttpGet]
+        public IHttpActionResult GetId(long id)
+        {
+            Author author = _db.Authors.Where(a => a.Id == id).Include(a => a.Songs).FirstOrDefault();
+
+            if (author == null)
+                return BadRequest("There is no author with this ID.");
+
+            IQueryable<Song> songs = (from aut_son in _db.AuthorSongs
+                                    where aut_son.AuthorId == id
+                                    join s in _db.Songs on aut_son.SongId equals s.Id
+                                    select s);
+            string userId = Microsoft.AspNet.Identity.IdentityExtensions.GetUserId(RequestContext.Principal.Identity);
+
+            AuthorFullVM model = new AuthorFullVM()
+            {
+                Id = author.Id,
+                Name = author.Name,
+                Songs = (new SongManager(_db)).MakeHeartedSong(songs, userId).ToList()
+            };
+
+            return Ok(model);
+        }
+
+    }
+}
